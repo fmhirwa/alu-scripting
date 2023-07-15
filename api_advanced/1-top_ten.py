bcb0929702c29ac971a4faa1f1a18bc40d7db556
fmhirwa <m.hirwa@alustudent.com>
2023-07-15 13:53:27 +0200

    Tasks

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""" Checks to see if the limits is met"""
+import requests
+
+def top_ten(subreddit):
+    """"Standard API request"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
+
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
+
+    if res.status_code != 200:
+        print(None)
+    else:
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]
